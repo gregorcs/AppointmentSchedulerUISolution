862e45d5f9be740fa6c8e4424146cc1a970dcf6b
Max <roomax667@gmail.com>
2022-12-09 22:49:00 +0100

    Small naming and linking fixes

--- AppointmentSchedulerUILibrary/AppointmentDTOs/GetAppointmentDTO.cs
+++ AppointmentSchedulerUILibrary/AppointmentDTOs/GetAppointmentDTO.cs
@@ -1,5 +1,6 @@
 ﻿using AppointmentSchedulerUILibrary.DataTransferObjects;
 using System.Collections;
+using System.ComponentModel.DataAnnotations;
 using System.Data;
 
 namespace AppointmentSchedulerUILibrary.AppointmentDTOs
@@ -7,11 +8,15 @@ namespace AppointmentSchedulerUILibrary.AppointmentDTOs
     public class GetAppointmentDTO
     {
         public long Id { get; set; }
+        
         public DateTime Date { get; set; }
+        [Display(Name = "Time")]
         public int TimeSlot { get; set; }
+        [Display(Name = "Approved")]
         public bool IsApproved { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
+        [Display(Name = "Procedure")]
         public string Name { get; set; }
         public string Description { get; set; }
         public IEnumerable<GetEmployeeDTO> Employees { get; set; }
