2c123f8858f08bc4eb388161bec572483bb4a64d
TrebroNm <81372891+TrebroNm@users.noreply.github.com>
2022-12-09 13:16:20 +0100

    Small change to GetAppointmentDTO

--- AppointmentSchedulerUILibrary/AppointmentDTOs/GetAppointmentDTO.cs
+++ AppointmentSchedulerUILibrary/AppointmentDTOs/GetAppointmentDTO.cs
@@ -1,36 +1,33 @@
-﻿using System.Collections;
+﻿using AppointmentSchedulerUILibrary.DataTransferObjects;
+using System.Collections;
 
-namespace AppointmentSchedulerUILibrary.AppointmentDTOs
+namespace AppointmentSchedulerServer.DataTransferObjects
 {
     public class GetAppointmentDTO
     {
         public long Id { get; set; }
-        public DateTime Time { get; set; }
+        public DateTime Date { get; set; }
         public int TimeSlot { get; set; }
         public bool IsApproved { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public IEnumerable EmployeeNameList { get; set; }
-        public string JobType { get; set; }
+        public IEnumerable<GetEmployeeDTO> Employees { get; set; }
 
         public GetAppointmentDTO()
         {
 
         }
 
-        public GetAppointmentDTO(long id, DateTime time, int timeSlot, bool isApproved, string name, string description, string email, string username)
+        public GetAppointmentDTO(long id, DateTime date, int timeSlot, bool isApproved, string name, string description)
         {
             Id = id;
-            Time = time;
+            Date = date;
             TimeSlot = timeSlot;
             IsApproved = isApproved;
             Name = name;
             Description = description;
-            EmployeeNameList = new ArrayList();
-            Username = username;
-            Email = email;
         }
     }
 }
