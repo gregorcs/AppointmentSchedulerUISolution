4754ba942df0add495c247abd4cb44ca1e208460
TrebroNm <81372891+TrebroNm@users.noreply.github.com>
2022-12-11 12:56:07 +0100

    Small fix

--- AppointmentSchedulerUILibrary/DataTransferObjects/GetEmployeeDTO.cs
+++ AppointmentSchedulerUILibrary/DataTransferObjects/GetEmployeeDTO.cs
@@ -2,7 +2,7 @@
 {
     public class GetEmployeeDTO
     {
-        public long Accounts_Id { get; set; }
+        public long Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public string Role { get; set; }
@@ -10,7 +10,7 @@
 
         public GetEmployeeDTO(long id, string username, string email, string role, int roomNumber)
         {
-            Accounts_Id = id;
+            Id = id;
             Username = username;
             Email = email;
             Role = role;
